9d9e6cb744e3d7db3a6027b7063da9bef0a29fe4
hegazy <ahmedrady03@gmail.com>
2026-09-15 00:43:29 +0300

    spring boot backend

---
+++ backend/e7gzly/src/main/java/com/example/e7gzly/E7gzlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.e7gzly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E7gzlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E7gzlyApplication.class, args);
+	}
+
+}
